cc8c5f769485a0d54c34ddfee744e05e3175f31c
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-15 13:20:32 +0200

    New changes

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{ color: 'red' }}>
+    <View className="items-center justify-center flex-1">
+      <Text className="text-5xl text-primary">
         Hello World!!!
       </Text>
     </View>
