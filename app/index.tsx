76163b66afcb2b38a040bd2b07dbc17ec1a7506e
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-15 10:46:12 +0200

    Add tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={{ color: 'red' }}>
+        Hello World!!!
+      </Text>
     </View>
   );
 }
