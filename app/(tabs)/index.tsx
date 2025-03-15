f83c09eaae3f1200d8e1c99ba13b8cf61089830e
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-15 14:35:03 +0200

    Setup index view

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,16 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import {Image, ScrollView, Text, View} from "react-native";
+import {images} from "@/constants/images";
+import {icons} from "@/constants/icons";
 
 export default function Index() {
   return (
-    <View className="items-center justify-center flex-1">
-      <Text className="text-5xl text-primary">
-        Hello World!!!
-      </Text>
+    <View className="flex-1 bg-primary">
+        <Image source={images.bg} className="absolute w-full z-0" />
+
+        <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}>
+            <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+        </ScrollView>
     </View>
   );
 }
