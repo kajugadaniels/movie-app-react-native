5e25a3bfcd67109c7d4b24d238f7cf7ff7500ecb
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-15 14:50:04 +0200

    Design SearchBar component

---
+++ components/SearchBar.tsx
@@ -0,0 +1,26 @@
+import { Image, StyleSheet, Text, TextInput, View } from 'react-native'
+import React from 'react'
+import { icons } from '@/constants/icons'
+import { useRouter } from 'expo-router'
+
+const SearchBar = () => {
+    const router = useRouter()
+    return (
+        <View className="flex-row items-center px-5 py-4 rounded-full bg-darl-200">
+            <Image source={icons.search} className='resize' resizeMode='contain' tintColor='#ab8bff' />
+
+            <TextInput
+                onPress={() => {}}
+                placeholder='Search'
+                value=''
+                onChangeText={() => {}}
+                placeholderTextColor="#a8b5db"
+                className='flex-1 ml-2 text-white'
+            />
+        </View>
+    )
+}
+
+export default SearchBar
+
+const styles = StyleSheet.create({})
